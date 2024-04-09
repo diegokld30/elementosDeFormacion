4602b54e1f78a3780440768823d2a89b00c7a2df
diegokld30 <dfercalderon@poligran.edu.co>
2024-04-09 16:19:24 -0500

    Se generan permisos para los roles que sean staff

--- roles/api/views.py
+++ roles/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from roles.models import Rol
 from roles.api.serializer import RolSerializer
+from roles.api.permissions import IsAdminOrReadOnly
 
 
 class RolApiViewSet(ModelViewSet):
-    serializer_class = RolSerializer
+    permission_classes = [IsAdminOrReadOnly] #Los permisos que se le dan para que muestre si es staff o no
+    serializer_class = RolSerializer #Cosas que vamos a mostrar
     queryset = Rol.objects.all()
