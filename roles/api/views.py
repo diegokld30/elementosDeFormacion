f3a34653df0fdad6f5c54a372e38428274bd5c55
diegokld30 <dfercalderon@poligran.edu.co>
2024-04-09 17:06:11 -0500

    Se genera un filtro para mostrar por state_rol sea True o false mediante el endpoint http://127.0.0.1:8000/api/rol/?state_rol=True

--- roles/api/views.py
+++ roles/api/views.py
@@ -3,8 +3,17 @@ from roles.models import Rol
 from roles.api.serializer import RolSerializer
 from roles.api.permissions import IsAdminOrReadOnly
 
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class RolApiViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly] #Los permisos que se le dan para que muestre si es staff o no
     serializer_class = RolSerializer #Cosas que vamos a mostrar
-    queryset = Rol.objects.all()
+    queryset = Rol.objects.all() #Me devuelve todos los elementos que tenga el modelo Rol
+    #lookup_field = 'slug' #Esto es para si quisiera hacer la busqueda de roles por slug y no por id, el por defecto lo hace por id
+
+    #queryset = Rol.objects.filter(state_rol=True)  # Le pedi que me devuelva solo los roles que tengan el estado en True
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['state_rol']
+
+
