d3d0605f4235e2c326cf2d774eea554d3e6c403c
Diego Fernando Calderon Silva <elingelindo@iMac-de-Diego.local>
2024-04-10 08:55:42 -0500

    Se elimino el campo slug del modelo movimiento ya que los movimientos van a ser los mismos siempre y el slug es para que sea unico en su campo

--- movimientos/admin.py
+++ movimientos/admin.py
@@ -3,4 +3,4 @@ from movimientos.models import Movimiento
 
 @admin.register(Movimiento)
 class MovimientoAdmin(admin.ModelAdmin):
-    list_display = ['tipo_movimiento', 'slug', 'persona_movimiento', 'fecha_creacion', 'fecha_modificacion']
+    list_display = ['tipo_movimiento', 'persona_movimiento', 'fecha_creacion', 'fecha_modificacion']
--- movimientos/models.py
+++ movimientos/models.py
@@ -6,7 +6,7 @@ from users.models import User
 
 class Movimiento(models.Model):
     tipo_movimiento = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True)
+    #slug = models.SlugField(max_length=255, unique=True) aca no me sirve por que los tipos de movimiento siempre son los mismos
     persona_movimiento = models.ForeignKey(User, on_delete=SET_NULL, null=True) #si se elimina el usuarios no se eliminara el movimiento, solo se pondra este espacio en null
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_modificacion = models.DateTimeField(auto_now_add=True)
