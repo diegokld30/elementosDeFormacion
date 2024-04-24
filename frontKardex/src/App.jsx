c142b10462421edbd4bf65a32edd3dc2a7fb203e
Diego Fernando Calderon Silva <elingelindo@iMac-de-Diego.local>
2024-04-23 21:11:00 -0500

    Se empezo a agregar secciones, hasta ahora solo se esta usando css, es por eso que se elimino todo del index.css y se empezo a crear de cero

--- frontKardex/src/App.jsx
+++ frontKardex/src/App.jsx
@@ -1,19 +1,49 @@
-import { AuthContextProvider, MyRoutes } from './index'
+import { AuthContextProvider, Dark, Light, MyRoutes } from './index'
+import styled, { ThemeProvider } from 'styled-components'
 import './App.css'
+import { createContext, useState } from 'react'
 
+export const ThemeContext = createContext(null)
 
 function App() {
-  
+  const [themeuse, setTheme] = useState('light')
+  const theme = themeuse === 'light' ? 'light' : 'dark'
+  const themeStyle = theme === 'light' ? Light : Dark
 
   return (
     <>
-      <AuthContextProvider>
-        <MyRoutes /> 
-        {/* aca en MyRoutes es donde estan mis paginas */}
-      </AuthContextProvider>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
 
+        <ThemeProvider theme={themeStyle}>
+            <AuthContextProvider>
+              <Container>
+                <section className="ContentSideBar">
+
+                </section>
+
+                <section className="ContentMenuambur">
+
+                </section>
+
+                <section className="ContentRoutes">
+
+                </section>
+
+                <MyRoutes /> 
+
+              </Container>
+            </AuthContextProvider>
+        </ThemeProvider>
+      </ThemeContext.Provider>
     </>
   )
 }
+
+const Container = styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+  background-color: ${({theme}) => theme.bgtotal};
+
+`
 
 export default App
