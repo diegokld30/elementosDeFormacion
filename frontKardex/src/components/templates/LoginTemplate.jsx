adcc89e81f13f44cf442f012bc4862b26104c32c
Diego Fernando Calderon Silva <elingelindo@iMac-de-Diego.local>
2024-05-02 21:40:26 -0500

    Se empieza a instalar tanStack Query

--- frontKardex/src/components/templates/LoginTemplate.jsx
+++ frontKardex/src/components/templates/LoginTemplate.jsx
@@ -1,12 +1,16 @@
 import styled from "styled-components"
+import { Btnsave, useUsuariosStore } from "../../index"
 
 export default function LoginTemplate() {
+  const {insertarUsuarioAdmin} = useUsuariosStore();
+  
   return (
     <Container>
-      <div>Hola Login</div>
+      <Btnsave titulo="Crear cuenta" bgcolor="#fff"/>
     </Container>
   )
 }
 const Container = styled.div`
+height: 100vh;
 
 `
